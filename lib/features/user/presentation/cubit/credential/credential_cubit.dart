9ca9a0019335f3c999f897c1444ed398e530bcbd
omer <omerkamil974@gmail.com>
2023-11-27 00:13:29 +0300

    get

--- lib/features/user/presentation/cubit/credential/credential_cubit.dart
+++ lib/features/user/presentation/cubit/credential/credential_cubit.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:whatsapp_clone_app/features/user/domain/entities/user_entity.dart';
 import 'package:whatsapp_clone_app/features/user/domain/usecases/credential/sign_in_with_phone_number_usecase.dart';
 import 'package:whatsapp_clone_app/features/user/domain/usecases/credential/verify_phone_number_usecsae.dart';
 import 'package:whatsapp_clone_app/features/user/domain/usecases/user/create_user_usecase.dart';
@@ -19,6 +20,27 @@ class CredentialCubit extends Cubit<CredentialState> {
     try {
       await verifyPhoneNumberUseCase.call(phoneNumber);
       emit(CredentialPhoneAuthSmsCodeReceived());
+    } on SocketException catch (_) {
+      emit(CredentialFailure());
+    } catch (_) {
+      emit(CredentialFailure());
+    }
+  }
+  Future<void> submitSmsCode({required String smsCode}) async {
+    try {
+      await signInWithPhoneNumberUseCase.call(smsCode);
+      emit(CredentialPhoneAuthProfileInfo());
+    } on SocketException catch (_) {
+      emit(CredentialFailure());
+    } catch (_) {
+      emit(CredentialFailure());
+    }
+  }
+
+  Future<void> submitProfileInfo({required UserEntity user}) async {
+    try {
+      await createUserUseCase.call(user);
+      emit(CredentialSuccess());
     } on SocketException catch (_) {
       emit(CredentialFailure());
     } catch (_) {
